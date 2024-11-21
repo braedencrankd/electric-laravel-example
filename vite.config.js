d059d15a5131704f6cb659068faa220df851378a
braedenf <braeden@crankd.nz>
2024-11-22 12:12:34 +1300

    Added laravel breeze + inertia

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
-import laravel from "laravel-vite-plugin";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode }) => {
-    //Load the env variables that are prefixed with VITE_
-    const env = loadEnv(mode, process.cwd(), "VITE_");
-    return {
-        server: {
-            hmr: {
-                protocol: "wss",
-                host: env.VITE_APP_URL,
-            },
-        },
-        plugins: [
-            laravel({
-                input: ["resources/css/app.css", "resources/js/app.js"],
-                refresh: true,
-            }),
-        ],
-    };
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.tsx',
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react(),
+    ],
 });
